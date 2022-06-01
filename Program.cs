b2df214ee7d35ea6acfaa2f33c56ef716c64eba3
kmerck88 <Kevin.Merck88@gmail.com>
2022-06-01 17:58:05 -0400

    Setting some logic off this program/app

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Deliverable2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+    Console.WriteLine("Hello, Welcome! How many people in your party? We can only accommodate party sizes of 6 and under at this time. The cost per guest is $9.99 today.");
+
+            int partySize = int.Parse(Console.ReadLine());
+
+            if(partySize >  6)
+            {
+                Console.WriteLine("Sorry, We can only accomadate parties of 6 or less  "); 
+            }
+
+            Console.WriteLine("ok, table for " + partySize + ", Please follow me to your table");
+            Console.WriteLine(" ");
+            Console.WriteLine("What would you all like to drink? We have coffee and water. Coffee is $3.00, and water is on the house.");
+
+
+
+
+
+
         }
     }
 }
